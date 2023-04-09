0fee1b0d7ad5f516a34add2f6d1f1500ea69a83c
Kirill Smirnov <62110814+Moon1it@users.noreply.github.com>
2023-04-09 13:32:38 +0700

    Refactored slider component using Swiper.js library.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 import "./index.html";
 import "./index.scss";
 import "./js/script.js";
+
+new Swiper(".reviews__content", {
+	slidesPerView: 4,
+	spaceBetween: 20,
+	// slidesPerGroup: 3,
+	loop: true,
+	// loopFillGroupWithBlank: true,
+	centerClide: true,
+	fade: true,
+	grabCursor: true,
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+		// dynamicBullets: true,
+	},
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},
+	breakpoints: {
+		0: {
+			slidesPerView: 1,
+		},
+		767.98: {
+			slidesPerView: 2,
+		},
+		991.98: {
+			slidesPerView: 3,
+		},
+		1300: {
+			slidesPerView: 4,
+		},
+	},
+});
