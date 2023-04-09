f56b3c7b356f5db5e119350813985d9c491046ca
Kirill Smirnov <62110814+Moon1it@users.noreply.github.com>
2023-04-09 23:29:29 +0700

    Added semantic tags and redesigned the background on the home page from blocks to background.

--- src/js/script.js
+++ src/js/script.js
@@ -13,101 +13,99 @@ if (homeLinks.length > 0) {
 		) {
 			const gotoBlock = document.querySelector(homeLink.dataset.goto);
 			const gotoBlockValue =
-				gotoBlock.getBoundingClientRect().top +
-				window.scrollY -
-				document.querySelector("header").offsetHeight;
+				gotoBlock.getBoundingClientRect().top + window.scrollY;
 			window.scroll({ top: gotoBlockValue, behavior: "smooth" });
 			e.preventDefault();
 		}
 	}
 }
 
-const reviewsPrev = document.querySelector(".reviews__prev");
-const reviewsNext = document.querySelector(".reviews__next");
-const reviewsList = document.querySelector(".reviews__list");
+// const reviewsPrev = document.querySelector(".reviews__prev");
+// const reviewsNext = document.querySelector(".reviews__next");
+// const reviewsList = document.querySelector(".reviews__list");
 
-if (reviewsPrev && reviewsNext && reviewsList) {
-	reviewsPrev.addEventListener("click", onReviewsPrevClick);
-	reviewsNext.addEventListener("click", onReviewsNextClick);
+// if (reviewsPrev && reviewsNext && reviewsList) {
+// 	reviewsPrev.addEventListener("click", onReviewsPrevClick);
+// 	reviewsNext.addEventListener("click", onReviewsNextClick);
 
-	function onReviewsPrevClick(e) {
-		const currentTranslateX = currentValueTranslateX();
-		const activeItemIndex = activeElementIndex();
+// 	function onReviewsPrevClick(e) {
+// 		const currentTranslateX = currentValueTranslateX();
+// 		const activeItemIndex = activeElementIndex();
 
-		reviewsList.children[activeItemIndex].classList.remove(
-			"reviews-item--active"
-		);
+// 		reviewsList.children[activeItemIndex].classList.remove(
+// 			"reviews-item--active"
+// 		);
 
-		if (!currentTranslateX) {
-			reviewsList.children[activeItemIndex - 1].classList.add(
-				"reviews-item--active"
-			);
-			reviewsList.style.transform = "translateX(606px)";
-			return;
-		}
+// 		if (!currentTranslateX) {
+// 			reviewsList.children[activeItemIndex - 1].classList.add(
+// 				"reviews-item--active"
+// 			);
+// 			reviewsList.style.transform = "translateX(606px)";
+// 			return;
+// 		}
 
-		if (activeItemIndex === 0) {
-			reviewsList.lastElementChild.classList.add("reviews-item--active");
-			reviewsList.style.transform = `translateX(${
-				-Math.floor(reviewsList.childElementCount / 2) * 606
-			}px)`;
-			return;
-		}
+// 		if (activeItemIndex === 0) {
+// 			reviewsList.lastElementChild.classList.add("reviews-item--active");
+// 			reviewsList.style.transform = `translateX(${
+// 				-Math.floor(reviewsList.childElementCount / 2) * 606
+// 			}px)`;
+// 			return;
+// 		}
 
-		reviewsList.children[activeItemIndex - 1].classList.add(
-			"reviews-item--active"
-		);
-		reviewsList.style.transform = `translateX(${currentTranslateX + 606}px)`;
-	}
+// 		reviewsList.children[activeItemIndex - 1].classList.add(
+// 			"reviews-item--active"
+// 		);
+// 		reviewsList.style.transform = `translateX(${currentTranslateX + 606}px)`;
+// 	}
 
-	function onReviewsNextClick(e) {
-		const currentTranslateX = currentValueTranslateX();
-		const activeItemIndex = activeElementIndex();
+// 	function onReviewsNextClick(e) {
+// 		const currentTranslateX = currentValueTranslateX();
+// 		const activeItemIndex = activeElementIndex();
 
-		reviewsList.children[activeItemIndex].classList.remove(
-			"reviews-item--active"
-		);
+// 		reviewsList.children[activeItemIndex].classList.remove(
+// 			"reviews-item--active"
+// 		);
 
-		if (!currentTranslateX) {
-			reviewsList.children[activeItemIndex + 1].classList.add(
-				"reviews-item--active"
-			);
-			reviewsList.style.transform = "translateX(-606px)";
-			return;
-		}
+// 		if (!currentTranslateX) {
+// 			reviewsList.children[activeItemIndex + 1].classList.add(
+// 				"reviews-item--active"
+// 			);
+// 			reviewsList.style.transform = "translateX(-606px)";
+// 			return;
+// 		}
 
-		if (activeItemIndex === reviewsList.childElementCount - 1) {
-			reviewsList.firstElementChild.classList.add("reviews-item--active");
-			reviewsList.style.transform = `translateX(${
-				Math.floor(reviewsList.childElementCount / 2) * 606
-			}px)`;
-			return;
-		}
+// 		if (activeItemIndex === reviewsList.childElementCount - 1) {
+// 			reviewsList.firstElementChild.classList.add("reviews-item--active");
+// 			reviewsList.style.transform = `translateX(${
+// 				Math.floor(reviewsList.childElementCount / 2) * 606
+// 			}px)`;
+// 			return;
+// 		}
 
-		reviewsList.children[activeItemIndex + 1].classList.add(
-			"reviews-item--active"
-		);
-		reviewsList.style.transform = `translateX(${currentTranslateX - 606}px)`;
-	}
+// 		reviewsList.children[activeItemIndex + 1].classList.add(
+// 			"reviews-item--active"
+// 		);
+// 		reviewsList.style.transform = `translateX(${currentTranslateX - 606}px)`;
+// 	}
 
-	function currentValueTranslateX() {
-		const transformValue = reviewsList.style.transform;
-		const value = Number(
-			transformValue.substring(
-				transformValue.indexOf("(") + 1,
-				transformValue.indexOf("px")
-			)
-		);
-		return value;
-	}
+// 	function currentValueTranslateX() {
+// 		const transformValue = reviewsList.style.transform;
+// 		const value = Number(
+// 			transformValue.substring(
+// 				transformValue.indexOf("(") + 1,
+// 				transformValue.indexOf("px")
+// 			)
+// 		);
+// 		return value;
+// 	}
 
-	function activeElementIndex() {
-		const elementIndex = Object.values(reviewsList.children).findIndex(
-			element => element.classList.contains("reviews-item--active")
-		);
-		return elementIndex;
-	}
-}
+// 	function activeElementIndex() {
+// 		const elementIndex = Object.values(reviewsList.children).findIndex(
+// 			element => element.classList.contains("reviews-item--active")
+// 		);
+// 		return elementIndex;
+// 	}
+// }
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function () {
